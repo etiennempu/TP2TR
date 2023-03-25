927fc58721c606e09930bb3b9b94725f6d9cfa29
etiennempu <etienne.lionnet@gmail.com>
2023-03-25 09:50:34 -0400

    fix

--- main.c
+++ main.c
@@ -120,25 +120,25 @@ int main(int argc, char** argv) {
 	
 	
 	LedUpdate(0);
-	delay(2);
+	sleep(2);
 	LedUpdate(1);
-	delay(2);
+	sleep(2);
 	LedUpdate(2);
-	delay(2);
+	sleep(2);
 	LedUpdate(3);
-	delay(2);
+	sleep(2);
 	LedUpdate(4);
-	delay(2);
+	sleep(2);
 	LedUpdate(5);
-	delay(2);
+	sleep(2);
 	LedUpdate(6);
-	delay(2);
+	sleep(2);
 	LedUpdate(7);
-	delay(2);
+	sleep(2);
 	LedUpdate(8);
-	delay(2);
+	sleep(2);
 	LedUpdate(9);
-	delay(2);
+	sleep(2);
 	LedUpdate(10);
     OuvrirServeur();
     OuvrirClient();
