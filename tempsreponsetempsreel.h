911d0176c2c153aac4dd4c480319df5cf0a93ffe
etiennempu <etienne.lionnet@gmail.com>
2023-04-14 19:05:00 -0400

    f

--- tempsreponsetempsreel.h
+++ tempsreponsetempsreel.h
@@ -4,10 +4,7 @@
 #include <time.h>
 
 
-time_t arrival_time[300];
-int arrivalIndex=0;
-time_t reaction_time[300];
-int reactionIndex=0;
+
 
 float calculateResponseTime(time_t arrival_time, time_t reaction_time);
 float calculateAverageResponseTime(float* response_times, int num_events);
